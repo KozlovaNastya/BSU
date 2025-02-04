e6d230190a827bdef0341fff114838533d121cb2
KozlovaNastya <nastya.kozlic.196@gmail.com>
2025-02-04 23:43:08 +0300

    Update main.cpp

--- fundamentals/extra/find_min_max/main.cpp
+++ fundamentals/extra/find_min_max/main.cpp
@@ -1,26 +1,17 @@
 #include <iostream>
 using namespace std;
-
 int main() {
     setlocale(LC_ALL, ".1251");
-
     int A, B, C;
-    cout << "Введите A, B, C" << endl;
+    cout << "Enter A, B, C" << endl;
     cin >> A >> B >> C;
-
-    // Проверка на равенство всех чисел
     if (A == B && B == C) {
-        cout << "Все числа равны: " << A << endl;
-        return 0; // Завершаем программу, так как нет смысла искать максимум/минимум
+        cout << "All numbers are equal: " << A << endl;
+        return 0;
     }
-
-    // Поиск наибольшего числа
     int maxVal = max(A, max(B, C));
-    cout << "Наибольшее число: " << maxVal << endl;
-
-    // Поиск наименьшего числа
+    cout << "Largest number: " << maxVal << endl;
     int minVal = min(A, min(B, C));
-    cout << "Наименьшее число: " << minVal << endl;
-
+    cout << "Smallest number: " << minVal << endl;
     return 0;
 }
