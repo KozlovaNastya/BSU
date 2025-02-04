890524bf440ddb9788a54016c101e60aa58d52d9
KozlovaNastya <nastya.kozlic.196@gmail.com>
2025-02-04 23:32:48 +0300

    Update main.cpp

--- fundamentals/extra/Qadratic_Equation/main.cpp
+++ fundamentals/extra/Qadratic_Equation/main.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, ".1251");
 	int a, b, c, D, x1, x2;
-	cout << "Введите a\n";
+	cout << "Enter a\n";
 	cin >> a;
-	cout << "Введите b\n";
+	cout << "Enter b\n";
 	cin >> b;
-	cout << "Введите с\n";
+	cout << "Enter с\n";
 	cin >> c;
 	D = b * b - 4 * a * c;
 	if (D > 0) {
@@ -16,7 +16,7 @@ int main() {
 		cout << "x1 = " << x1 << "\nx2 = " << x2;
 	}
 	if (D < 0)
-		cout << "Решений нет";
+		cout << "no solutions";
 	if (D == 0) {
 		x1 = (-b / 2 * a);
 		cout << "x = " << x1;
