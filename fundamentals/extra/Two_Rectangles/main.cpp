19309d7334cfc1a2d99a160a004d37534e36b878
KozlovaNastya <nastya.kozlic.196@gmail.com>
2025-02-04 23:40:38 +0300

    Update main.cpp

--- fundamentals/extra/Two_Rectangles/main.cpp
+++ fundamentals/extra/Two_Rectangles/main.cpp
@@ -3,22 +3,22 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, ".1251");
 	int x, y, a, b;
-	cout << "Введите ширину 1 прямоугольника" << endl;
+	cout << "Enter the width of 1 rectangle" << endl;
 	cin >> x;
-	cout << "Введите длинну 1 прямоугольника" << endl;
+	cout << "Enter the length of 1 rectangle" << endl;
 	cin >> y;
-	cout << "Введите ширину 2 прямоугольника" << endl;
+	cout << "Enter a width of 2 rectangles" << endl;
 	cin >> a;
-	cout << "Введите длинну 2 прямоугольника" << endl;
+	cout << "Enter the length of 2 rectangles" << endl;
 	cin >> b;
 	if (x < a && y < b)
-		cout << "Можно во 2 поместить 1";
+		cout << "You can put 1 in 2";
 	else if (x < b && y < a)
-			cout << "Можно во 2 поместить 1";
+			cout << "You can put 1 in 2";
 	else if (a < x && b < y)
-			cout << "Можно в 1 поместить 2";
+			cout << "You can put 2 in 1";
 	else if (a < y && b < x)
-			cout << "Можно в 1 поместить 2";
-	else cout << "Нельзя";
+			cout << "You can put 2 in 1";
+	else cout << "Impossible";
 	return 0;
 }
