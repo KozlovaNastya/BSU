98a8afab03e849fb42fbcfaf5dcededfc2d644d9
KozlovaNastya <nastya.kozlic.196@gmail.com>
2025-02-04 14:28:31 +0300

    Update Flower.h

--- fundamentals/labs/lab10/Flower.h
+++ fundamentals/labs/lab10/Flower.h
@@ -13,41 +13,25 @@ protected:
 	ListItem* First;
 public:
 	ListOfFlowers();
-	// конструктор
 	ListOfFlowers(const string&, const string&, const float&);
-	// конструктор копировани€
 	ListOfFlowers(const ListOfFlowers&);
-	//получение первого элемента
 	ListItem* getFirst();
-	// добавление нового элемента в начало списка
 	void InsertFirst(const string&, const string&, const float&);
-	//добавление нового элемента в конец списка
 	void InsertLast(const string&, const string&, const float&);
-	// удаление первого элемента
 	bool DeleteFirst();
-	// удаление последнего элемента
 	bool DeleteLast();
-	// посмотреть первый элемент списка
 	void Top() const;
-	// печать элементов списка
 	void ListPrint() const; 
-	// выполнить действи€ над всеми элементами списка, не измен€€ его
 	void ForEach(void (ListOfFlowers::* callback)(const ListItem&) const) const;
-	// выполнить действи€ над всеми элементами списка, измен€€ его
     void ForEach(void(ListOfFlowers::* callback)(ListItem&));
-	// оператор присваивани€
 	const ListOfFlowers& operator = (const ListOfFlowers&); 
-	// деструктор
 	virtual~ListOfFlowers(); 
-	// удаление элементов списка
 	void Erase(); 
-	//удаление элемента по значению
 	bool DeleteByValue(const string& flowerName);
-	//поиск элемента по значению с подсчетом
 	bool SearchByValueWithCount(const string& flowerName, int& comparisonsCount) const;
 	void PrintFlowerInfo(const ListItem&) const;
 	void IncreaseHeight(ListItem&);
 protected:
-	void Clone(const ListOfFlowers&); // клонирование
+	void Clone(const ListOfFlowers&);
 };
 #endif
