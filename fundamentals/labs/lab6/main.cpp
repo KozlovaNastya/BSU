218cead3fe8834fd40c447834da472004ab1798c
KozlovaNastya <nastya.kozlic.196@gmail.com>
2025-02-04 12:44:45 +0300

    Update main.cpp

--- fundamentals/labs/lab6/main.cpp
+++ fundamentals/labs/lab6/main.cpp
@@ -2,44 +2,42 @@
 #include <fstream>
 #include <string>
 using namespace std;
-//функци€ дл€ проверки на строговозрастающую подстроку
 bool Proverka(const string& subline) { 
 	for (int i = 1; i < subline.length(); i++) {
-		if (subline[i - 1] >= subline[i]) // ≈сли текущий символ не больше предыдущего
-			return false; // ¬озвращаем false, если не строго возрастающа€
+		if (subline[i - 1] >= subline[i])
+			return false;
 	}
-	return true; // ¬озвращаем true, если слово строго возрастающее
+	return true;
 }
-//функци€ нахождени€ максимальной строго возрастающей подстроки
 string MaxsubLine(const string& line) { 
-	string subline; //переменна€ дл€ подстроки
-	string maxsubline; //переменна€ дл€ максимальной подстроки
-	int pos = 0; //переменна€ дл€ позиции начала подстроки
-	while (pos < line.length()) { //разбиваем на подстроки
-		size_t space = line.find(' ', pos); //позици€ пробела
+	string subline;
+	string maxsubline;
+	int pos = 0;
+	while (pos < line.length()) {
+		size_t space = line.find(' ', pos);
 		if (space == string::npos)
-			space = line.length(); // ≈сли пробелов больше нет, берем до конца строки
-		subline = line.substr(pos, space - pos); //копируем подстроку
-		pos = space + 1; //переходим к следующему слову
-		if (Proverka(subline) && maxsubline.length() < subline.length()) //проверка на строгое возрастание и максимальную подстроку
+			space = line.length();
+		subline = line.substr(pos, space - pos);
+		pos = space + 1;
+		if (Proverka(subline) && maxsubline.length() < subline.length())
 				maxsubline = subline;
 		}
 	return maxsubline;
 }
 int main() {
 	setlocale(LC_ALL, ".1251");
-	ifstream in("input.txt"); //открываем файл дл€ чтени€
+	ifstream in("input.txt");
 	if (!in.is_open()) {
-		cerr << "ERROR with input.txt"; //выполн€ем проверку на открытие файла
+		cerr << "ERROR with input.txt";
 		return 1;
 	}
-	string line; //переменна€ дл€ строки из файла
-	int count = 0; //счетчик дл€ выведеных строк
-	while (getline(in, line) && count < 10) { //читаем строки пока в файле есть информаци€ и пока не выведем 10 строк
+	string line;
+	int count = 0;
+	while (getline(in, line) && count < 10) {
 		string subline = MaxsubLine(line);
 		if (!subline.empty())
-			cout << ++count << ". ћаксимальна€ строговозрастающа€ подстрока: " << subline << endl << "¬ строке: " << line << endl << endl;
+			cout << ++count << ". Maximum strictly increasing substring: " << subline << endl << "In line: " << line << endl << endl;
 	}
-	in.close(); //закрываем файл
+	in.close();
 	return 0;
 }
